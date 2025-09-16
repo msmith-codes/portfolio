d7946d4ea298e05baba79942cca22aa7d9fdacb0
msmith-codes <msmith.softwaredev@gmail.com>
2025-09-16 14:01:19 -0400

    Updated loan calc

--- public/work/web-programming-1/in_class/loan_calculator/loan_calc.js
+++ public/work/web-programming-1/in_class/loan_calculator/loan_calc.js
@@ -6,9 +6,8 @@ function calculate(event)
     const aprInput = document.getElementById("aprInput");
     const amtBorrowInput = document.getElementById("amtBorrowInput");
     const numMonthsInput = document.getElementById("numMonthsInput");
-    const numCompoundsInput = document.getElementById("numCompoundsInput"); 
     const result = document.getElementById("result");
-
+    
     const x = (1 + aprInput) ** numMonthsInput;
     const payment = (amtBorrowInput * aprInput * x) / ( x - 1);
 
